a870e8ade13cbebd05f0cdc711bc75216691b850
Marcus Kitchen <marcusdkitchen@gmail.com>
2023-11-02 15:07:11 -0400

    objLAB

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your solution in this file!
+let employee = {name: "Sam", streetAddress: "11 Broadway"};
+function updateEmployeeWithKeyAndValue(employee, name, value) {let newEmployee = {...employee};
+newEmployee[name] = value;
+return newEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {employee.streetAddress = "12 Broadway";
+return employee}
+function deleteFromEmployeeByKey(employee, key) {let modEmployee = {...employee};
+delete modEmployee.name;
+return modEmployee};
+function destructivelyDeleteFromEmployeeByKey(employee, key) {delete employee.name;
+return employee}
